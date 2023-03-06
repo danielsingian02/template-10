fdf9655a017d3434b45ef368ee4dd245e95fcc2d
Daniel Singian <daniel.singian02@gmail.com>
2023-03-06 16:44:48 +0800

    added escapes

--- page-templates/home-page.php
+++ page-templates/home-page.php
@@ -28,6 +28,10 @@ $first_image = $banner_section['first_image'] ?: "https://raw.githubusercontent.
 $second_image = $banner_section['second_image'] ?: "https://raw.githubusercontent.com/danielsingian02/template_10/main/assets/images/Asset%209.png";
 $third_image = $banner_section['third_image'] ?: "https://raw.githubusercontent.com/danielsingian02/template_10/main/assets/images/Asset%2010.png";
 
+echo "<pre>";
+var_dump($banner_section);
+echo "</pre>";
+
 get_header();
 ?>
 
@@ -39,28 +43,28 @@ get_header();
         <section class="banner">
             <div class="banner-wrapper">
                 <h2 class="banner-title">
-                    <?php echo $banner_section['text_banner']; ?>
+                    <?php echo acf_esc_html($banner_section['text_banner']); ?>
                 </h2>
 
-                <div class="banner-hr" style="background-color: <?php echo $banner_section['border_color'] ?>"></div>
+                <div class="banner-hr" style="background-color: <?php echo esc_attr($banner_section['border_color']) ?>"></div>
             </div>
             <div class="banner-wrapper__image-grid">
                 <div class="image-grid">
                     <?php if($banner_section['display_first_image']) : ?>
                         <div class="first-image">
-                            <img src="<?php echo $first_image ?>" alt="">
+                            <img src="<?php echo esc_url($first_image) ?>" alt="">
                         </div>
                     <?php endif; ?>
 
                     <?php if($banner_section['display_second_image']) : ?>
                     <div class="second-image">
-                        <img src="<?php echo $second_image ?>" alt="">
+                        <img src="<?php echo esc_url($second_image) ?>" alt="">
                     </div>
                     <?php endif; ?>
 
                     <?php if($banner_section['display_third_image']) : ?>
                     <div class="third-image">
-                        <img src="<?php echo $third_image ?>" alt="">
+                        <img src="<?php echo esc_url($third_image) ?>" alt="">
                     </div>
                     <?php endif; ?>
                 </div>
