242edc15e1e2835fa2c7f5416121311b7fb52507
Daniel Singian <daniel.singian02@gmail.com>
2023-03-07 09:00:31 +0800

    fixed the background image section

--- template-parts/components/background_image.php
+++ template-parts/components/background_image.php
@@ -1,6 +1,6 @@
 <?php
 
-$background_image = $args["background_image"] ?? 'https://raw.githubusercontent.com/danielsingian02/template_10/main/assets/images/Asset%2012%201.png';
+$background_image = $args["data"]["background_image"] ?: 'https://raw.githubusercontent.com/danielsingian02/template_10/main/assets/images/Asset%2012%201.png';
 
 ?>
 
